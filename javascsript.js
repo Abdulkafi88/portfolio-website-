de0fc129b135546296099cf4ac3aff071bb1226c
Abdulkafi88 <123754218+Abdulkafi88@users.noreply.github.com>
2023-08-15 03:25:21 -0700

    Update javascsript.js

--- javascsript.js
+++ javascsript.js
@@ -1,23 +1,20 @@
 const chagne = document.querySelector("#change")
 const randomImgContainer = document.querySelector(".left-about")
-
+const btnBackground = document.querySelector(".right-about .btn-main")
+const btnBackground2 = document.querySelector(".form  .btn-submit")
+const service = document.querySelectorAll(".service")
 chagne.addEventListener("click", (e) => {
   const target = e.target
   document.body.classList.toggle("dark-theme")
-
+  if (target) {
+    btnBackground.style.backgroundColor = "#27ae60"
+    btnBackground.style.color = "white"
+    btnBackground2.style.backgroundColor = "#27ae60"
+    btnBackground2.style.color = "white"
+   service.forEach(items => {
+    
+   });
+  }
   console.log()
 })
 
-function random() {
-  let getRandomImg = [
-    `img/Screenshot 2023-08-13 at 5.06.56 AM.png`,
-    `img/Screenshot 2023-08-13 at 5.05.58 AM.png`,
-  ]
-  const pickRandomImg = Math.floor(Math.random() * getRandomImg.length)
-  const NewImg = document.createElement("img")
-  NewImg.src = getRandomImg[pickRandomImg]
-  randomImgContainer.appendChild(NewImg)
-}
-setTimeout(() => {
-  random()
-}, 10)
